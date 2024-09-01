f84712374e20e06426866e7b759927bb123064fe
Germán Salina <gersalina28@gmail.com>
2024-09-01 19:33:13 -0300

    added eb package configuration

--- coffee_shop/wsgi.py
+++ coffee_shop/wsgi.py
@@ -4,7 +4,7 @@ import environ
 
 from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coffee_shop.settings")
-os.environ['DJANGO_SETTINGS_MODULE'] = 'coffee_shop.settings'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coffee_shop.settings")
+# os.environ['DJANGO_SETTINGS_MODULE'] = 'coffee_shop.settings'
 
 application = get_wsgi_application()
